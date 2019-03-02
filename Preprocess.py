f9855f2283da9fecced315ba72d3bf1cf89fec0d
Kyle Lavorato <kyle.lavorato@queensu.ca>
2019-03-01 21:31:50 -0500

    Updated preprocessing script to convert emojis to usable text in _ notation and remove duplicate in the same tweet

--- Preprocess.py
+++ Preprocess.py
@@ -1,3 +1,5 @@
+import emoji
+
 def parse_dataset(fp):
     '''
     Loads the dataset .txt file with label-tweet on each line and parses the dataset.
@@ -15,8 +17,10 @@ def parse_dataset(fp):
                 label = int(line.split("\t")[1])
                 tweet = line.split("\t")[2]
                 y.append(label)
-                tweet_pp1 = removeURLs(tweet)  # Preprocess stage 1
-                corpus.append(tweet_pp1)
+                tweet_PP1 = removeURLs(tweet)  # Preprocess stage 1
+                tweet_PP2 = removeEmoji(tweet_PP1)
+                corpus.append(tweet_PP2)
+
 
     return corpus, y
 
@@ -39,6 +43,24 @@ def removeURLs(tweet):
             processedTweet.append(word)
     newTweet = ' '.join(map(str, processedTweet))
     return newTweet
+
+
+def removeEmoji(tweet):
+    processedTweet = []
+    # Ensure each emoji is only in the tweet one time to prevent spam/overfitting
+    # People often put several of the same emoji in a row
+    usedEmoji = []
+    for char in tweet:
+        if char in emoji.UNICODE_EMOJI:
+            if char not in usedEmoji:
+                usedEmoji.append(char)
+                emojiText = emoji.demojize(char).replace(":","")
+                processedTweet.append(emojiText)
+        else:
+            processedTweet.append(char)
+    newTweet = ''.join(map(str, processedTweet))
+    return newTweet
+
 
 
 def printCorpus(corpus, y):
